6614dc3bb8526ffac26126af3cf9f81f26ee1b64
eljoshy <josuealejandrocifuentes@gmail.com>
2024-08-28 22:44:49 -0600

    SchoolManagement

---
+++ src/Estudiante.java
@@ -0,0 +1,12 @@
+class Estudiante extends Persona {
+    private String estado;
+
+    public Estudiante(int id, String nombre, String apellido, String fechaDeNacimiento, String estado) {
+        super(id, nombre, apellido, fechaDeNacimiento);
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
